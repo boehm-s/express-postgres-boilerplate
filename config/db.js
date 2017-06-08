6a3f19ba9fc47cb7def57d29e65e9d155d655bf0
erwan boehm <boehm_e@etna-alternance.net>
2017-06-08 14:14:07 +0200

    postgres/knex setup

--- config/db.js
+++ config/db.js
@@ -1,3 +1,4 @@
 const knex = require('knex')(require('./db_config')[process.env.NODE_ENV || "development"]);
 const bookshelf = require('bookshelf')(knex);
+console.log("HERE");
 module.exports = bookshelf;
